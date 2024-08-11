039b760e110931854398b029bae9235e341d05eb
Rishi Mathur <rishimathur14@gmail.com>
2024-08-11 15:02:47 +0530

    Update 00-run.sh
    updated qiskit

--- stage4/07-enable-qiskit/00-run.sh
+++ stage4/07-enable-qiskit/00-run.sh
@@ -4,6 +4,6 @@ echo "starting qiskit setup"
 # Copy the raspi-config file to the desired locations
 cp  config/raspi-config  ${ROOTFS_DIR}/usr/bin/raspi-config
 cp  config/raspi-config  ${ROOTFS_DIR}/etc/init.d/raspi-config
-SUDO_USER="${FIRST_USER_NAME}" cp /bin/*   ${ROOTFS_DIR}/usr/bin/
-SUDO_USER="${FIRST_USER_NAME}" mkdir -p  ${ROOTFS_DIR}/usr/config && cp config/config-env/*   ${ROOTFS_DIR}/usr/config
+cp /bin/*   ${ROOTFS_DIR}/usr/bin/
+mkdir -p  ${ROOTFS_DIR}/usr/config && cp config/config-env/*   ${ROOTFS_DIR}/usr/config
 echo "end qiskit setup"
