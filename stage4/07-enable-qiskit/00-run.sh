ca9195d78a7910f52a88d0c60178ea82f8214e18
Rishi Mathur <rishimathur14@gmail.com>
2024-08-21 19:15:35 +0530

    Changes for qiskit
    Stage 5 changes

--- stage4/07-enable-qiskit/00-run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-echo "starting qiskit setup"
-# Copy the raspi-config file to the desired locations
-cp  config/raspi-config  ${ROOTFS_DIR}/usr/bin/raspi-config
-cp  config/raspi-config  ${ROOTFS_DIR}/etc/init.d/raspi-config
-cp /bin/*   ${ROOTFS_DIR}/usr/bin/
-mkdir -p  ${ROOTFS_DIR}/usr/config && cp config/config-env/*   ${ROOTFS_DIR}/usr/config
-echo "end qiskit setup"
--- stage5/prerun.sh
+++ stage5/prerun.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
-
+chmod +x qiskit-setup-run.sh
+./qiskit-setup-run.sh
 if [ ! -d "${ROOTFS_DIR}" ]; then
 	copy_previous
 fi
