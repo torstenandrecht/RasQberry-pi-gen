d38f27aee0de364b761e5d0b6f4e01c614b5f085
Rishi Mathur <rishimathur14@gmail.com>
2024-08-22 16:51:57 +0530

    Hardcoding to Config
    Move the hardcoding to Config file

--- stage5/qiskit-setup-run.sh
+++ stage5/qiskit-setup-run.sh
@@ -1,16 +1,13 @@
 #!/bin/bash -e
 
 # Clone the Git repository
-GIT_REPO="https://github.com/rishimathur14/RasQberry-Two.git"
-GIT_BRANCH="30-sw-platform-rishi"
-CLONE_DIR="/tmp/RasQberry-Two"
+export CLONE_DIR="/tmp/RasQberry-Two"
 
-if [ ! -d "$CLONE_DIR" ]; then
+if [ ! -d "${CLONE_DIR}" ]; then
     git clone --branch $GIT_BRANCH $GIT_REPO $CLONE_DIR
 fi
 
-# Copy raspi-config to bin 
-wget https://raw.githubusercontent.com/rishimathur14/RasQberry-raspi-config/30-sw-platform-rishi/raspi-config -O raspi-config
+wget ${RASP_WGET} -O raspi-config
 # Copy the raspi-config file to the desired locations
  cp  raspi-config  ${ROOTFS_DIR}/usr/bin/raspi-config
  cp  raspi-config  ${ROOTFS_DIR}/etc/init.d/raspi-config
