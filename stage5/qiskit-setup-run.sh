681631bd410effc76a259397adcbe7c0f1e969f6
Rishi Mathur <rishimathur14@gmail.com>
2024-08-24 05:42:45 +0530

    Update qiskit-setup-run.sh
    setting up virtual env for user

--- stage5/qiskit-setup-run.sh
+++ stage5/qiskit-setup-run.sh
@@ -18,6 +18,7 @@ chmod +x  ${ROOTFS_DIR}/usr/bin/
 chmod +x  ${ROOTFS_DIR}/usr/config 
 # Clean up the temporary clone directory if needed
 # Install Qiskit using pip
+echo "install qiskit for ${FIRST_USER_NAME} user
 mkdir -p /home/${FIRST_USER_NAME}/qiskit_env
 
 python3 -m venv /home/${FIRST_USER_NAME}/qiskit_env
@@ -29,6 +30,7 @@ pip install --upgrade pip
 pip install qiskit
 
 deactivate
-
+cp -r /home/rasqberry/qiskit_env ${ROOTFS_DIR}/home/rasqberry/
 echo "source /home/${FIRST_USER_NAME}/qiskit_env/bin/activate" >> ${ROOTFS_DIR}/etc/skel/.bashrc
+echo "install qiskit end for ${FIRST_USER_NAME}"
 rm -rf $CLONE_DIR
