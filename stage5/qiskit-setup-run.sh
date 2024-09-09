4ca3989e42129669c1e310773fda31e849c5c7d8
Rishi Mathur <rishimathur14@gmail.com>
2024-09-09 19:22:35 +0530

    Update qiskit-setup-run.sh

--- stage5/qiskit-setup-run.sh
+++ stage5/qiskit-setup-run.sh
@@ -48,7 +48,6 @@ deactivate
 cp -r /home/${FIRST_USER_NAME}/.local  "${ROOTFS_DIR}"/home/${FIRST_USER_NAME}/
 cp  -r /home/${FIRST_USER_NAME}/$REPO "${ROOTFS_DIR}"/home/${FIRST_USER_NAME}/
 cp  -r /home/${FIRST_USER_NAME}/$REPO  "${ROOTFS_DIR}"/usr/venv
-echo "source /home/${FIRST_USER_NAME}/$REPO/venv/$STD_VENV/bin/activate" >> "${ROOTFS_DIR}"/home/"${FIRST_USER_NAME}"/.bashrc
 export LINE=". /usr/config/setup_qiskit_env.sh"
 echo "$LINE" >> ${ROOTFS_DIR}/etc/skel/.bashrc
 echo "install qiskit end for ${FIRST_USER_NAME}"
