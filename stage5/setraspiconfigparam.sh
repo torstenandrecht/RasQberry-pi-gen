34dc66168faee2bd30c4c9638fc1778f27228aa2
Rishi Mathur <rishimathur14@gmail.com>
2024-10-10 10:32:02 +0530

    Enable VNC
    Testing Enabling VNC

--- stage5/setraspiconfigparam.sh
+++ stage5/setraspiconfigparam.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-# Enable VNC
-raspi-config nonint do_vnc 0
+# Enable VNC non-interactively
+on_chroot << EOF
+raspi-config nonint do_vnc 1
+EOF
+
+# Set screen resolution (e.g., 1080p)
+echo "hdmi_force_hotplug=1" >> ${ROOTFS_DIR}/boot/config.txt
+echo "hdmi_group=2" >> ${ROOTFS_DIR}/boot/config.txt
+echo "hdmi_mode=82" >> ${ROOTFS_DIR}/boot/config.txt
