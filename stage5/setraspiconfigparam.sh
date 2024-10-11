cf7c1816e0595e8ed019768ac00bd4844662105e
Rishi Mathur <rishimathur14@gmail.com>
2024-10-11 23:10:30 +0530

    Update setraspiconfigparam.sh
    Enable VNC

--- stage5/setraspiconfigparam.sh
+++ stage5/setraspiconfigparam.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+# Enable VNC and set boot to desktop with autologin for any user
 
-# Enable VNC
-raspi-config nonint do_vnc 0
+# Install VNC
+on_chroot << EOF
+apt-get install -y realvnc-vnc-server
+systemctl enable vncserver-x11-serviced.service
+EOF
+
+# Modify lightdm.conf to leave autologin-user commented out (no default user)
+on_chroot << EOF
+sed -i 's/^autologin-user=/#autologin-user=/' /etc/lightdm/lightdm.conf
+EOF
+
+
+# Optional: Set resolution for headless operation
+echo "hdmi_force_hotplug=1" >> ${ROOTFS_DIR}/boot/config.txt
+echo "hdmi_group=2" >> ${ROOTFS_DIR}/boot/config.txt
+echo "hdmi_mode=85" >> ${ROOTFS_DIR}/boot/config.txt
