781c4e4453718b9607074b71b9bb141b6271a06d
Rishi Mathur <rishimathur14@gmail.com>
2024-10-14 23:25:18 +0530

    Updating for VNC
    VNC enablement using raspi non interactive mode

--- stage5/setraspiconfigparam.sh
+++ stage5/setraspiconfigparam.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
+# Enable VNC and set boot to desktop with autologin for any user using raspi-config in non-interactive mode
 
-# Enable VNC non-interactively
+# Enable VNC through raspi-config (non-interactive mode)
 on_chroot << EOF
-raspi-config nonint do_vnc 1
+raspi-config nonint do_vnc 0
 EOF
 
-# Set screen resolution (e.g., 1080p)
+# Set boot option to desktop with autologin (non-interactive mode)
+on_chroot << EOF
+raspi-config nonint do_boot_behaviour B4
+EOF
+
+# Modify lightdm.conf to leave autologin-user commented out (no default user)
+on_chroot << EOF
+sed -i 's/^autologin-user=/#autologin-user=/' /etc/lightdm/lightdm.conf
+EOF
+
+# Optional: Set resolution for headless operation
 echo "hdmi_force_hotplug=1" >> ${ROOTFS_DIR}/boot/config.txt
 echo "hdmi_group=2" >> ${ROOTFS_DIR}/boot/config.txt
-echo "hdmi_mode=82" >> ${ROOTFS_DIR}/boot/config.txt
+echo "hdmi_mode=85" >> ${ROOTFS_DIR}/boot/config.txt
+
+
+
