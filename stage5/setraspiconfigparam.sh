52be4e4463671c0c8388490e92f8d1449313e163
Rishi Mathur <rishimathur14@gmail.com>
2024-10-16 10:53:18 +0530

    Update for VNC
    update VNC enablement with raspi-config

--- stage5/setraspiconfigparam.sh
+++ stage5/setraspiconfigparam.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-<<<<<<< HEAD
 # Enable VNC and set boot to desktop with autologin for any user using raspi-config in non-interactive mode
-
-# Enable VNC through raspi-config (non-interactive mode)
 on_chroot << EOF
 raspi-config nonint do_vnc 0
 EOF
@@ -13,34 +10,12 @@ raspi-config nonint do_boot_behaviour B4
 EOF
 
 # Modify lightdm.conf to leave autologin-user commented out (no default user)
-on_chroot << EOF
-sed -i 's/^autologin-user=/#autologin-user=/' /etc/lightdm/lightdm.conf
-EOF
+#on_chroot << EOF
+#sed -i 's/^autologin-user=/#autologin-user=/' /etc/lightdm/lightdm.conf
+#EOF
 
 # Optional: Set resolution for headless operation
 echo "hdmi_force_hotplug=1" >> ${ROOTFS_DIR}/boot/config.txt
 echo "hdmi_group=2" >> ${ROOTFS_DIR}/boot/config.txt
 echo "hdmi_mode=85" >> ${ROOTFS_DIR}/boot/config.txt
 
-
-
-=======
-# Enable VNC and set boot to desktop with autologin for any user
-
-# Install VNC
-on_chroot << EOF
-apt-get install -y realvnc-vnc-server
-systemctl enable vncserver-x11-serviced.service
-EOF
-
-# Modify lightdm.conf to leave autologin-user commented out (no default user)
-on_chroot << EOF
-sed -i 's/^autologin-user=/#autologin-user=/' /etc/lightdm/lightdm.conf
-EOF
-
-
-# Optional: Set resolution for headless operation
-echo "hdmi_force_hotplug=1" >> ${ROOTFS_DIR}/boot/config.txt
-echo "hdmi_group=2" >> ${ROOTFS_DIR}/boot/config.txt
-echo "hdmi_mode=85" >> ${ROOTFS_DIR}/boot/config.txt
->>>>>>> cd6c9aaca3627b53f714ac2b893d5e20e3100909
