618ce306f993033f4ae10deeb4bf99b80e044503
Rishi Mathur <rishimathur14@gmail.com>
2024-10-28 22:22:41 +0530

    Reverted back
    Reverted back

--- stage5/02-enable-vnc/00-run.sh
+++ stage5/02-enable-vnc/00-run.sh
@@ -2,11 +2,5 @@
 # Enable vnc server start at boot
 
 on_chroot << EOF
-	raspi-config nonint do_vnc 1
-	systemctl enable wayvnc.service
-	systemctl start wayvnc.service
+	SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_vnc 0 
 EOF
-# Optional: Set resolution for headless operation
-echo "hdmi_force_hotplug=1" >> ${ROOTFS_DIR}/boot/config.txt
-echo "hdmi_group=2" >> ${ROOTFS_DIR}/boot/config.txt   # Set for monitor (DMT)
-echo "hdmi_mode=82" >> ${ROOTFS_DIR}/boot/config.txt   # 1920x1080 @ 60Hz
