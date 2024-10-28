c361215158d49eb459ffed85c326d3bfb593aa0d
JanLahmann <jan@lahmann-online.de>
2024-10-28 22:47:34 +0100

    fix desktop autologin / vnc; chmod 00-run-sh

--- stage5/03-desktop-autologin/00-run.sh
+++ stage5/03-desktop-autologin/00-run.sh
@@ -3,5 +3,5 @@
 # See https://github.com/raspberrypi/documentation/blob/develop/documentation/asciidoc/computers/configuration/raspi-config.adoc for alternatives
 
 on_chroot << EOF
-	SUDO_USER="${FIRST_USER_NAME}" sudo raspi-config nonint do_boot_behaviour B4 
+	SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_boot_behaviour B4 
 EOF
